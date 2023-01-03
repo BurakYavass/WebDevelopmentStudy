7e2336e38041a2bad253fdc398af0f7f9edd4aac
BurakYavas <burakyavas26@gmail.com>
2023-01-04 00:39:50 +0300

    Ecma Script let and const

---
+++ scope.js
@@ -0,0 +1,31 @@
+var sayi = 10
+var sehir = "Ankara"
+
+sayi = 11
+sehir = "Istanbul"
+
+var sehir = "Adana"
+
+// let city = "Ankara"
+
+// function giveMessage(){
+//     var name = "Burak"
+//     console.log(sehir)
+// }
+
+// console.log(sayi,sehir)
+// console.log(city)
+
+//giveMessage()
+//console.log(name)
+
+// for(let i=1;i<10;i++){
+
+// }
+
+// console.log(i)
+
+
+const surName = "Yavas"
+
+console.log(surName)
