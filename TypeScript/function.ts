a634c0cf3f4c136ecd8dbeb3a62478d16b3ad649
BurakYavas <burakyavas26@gmail.com>
2023-01-05 04:32:14 +0300

    Default and Optional Parameters

---
+++ TypeScript/function.ts
@@ -0,0 +1,31 @@
+function topla(x:number,y:number):number{
+    return x+y;
+
+}
+
+function topla1(x,y){
+    return x+y;
+}
+
+let topla2 = function(x:number,y:number):number{
+    return x+y;
+}
+
+console.log(topla(2,3))
+console.log(topla2(5,2))
+console.log(topla1(1,"Ankara"))
+
+function topla3(x:number,y:number = 4):number{
+    return x+y;
+}
+
+console.log(topla3(3))
+
+function topla4(x:number,y?:number):number{
+    if(y) {
+        return x+y;
+    }
+    return x;
+}
+
+console.log(topla4(10))
--- TypeScript/intro.js
+++
@@ -1,5 +0,0 @@
-function greeter(name) {
-    return "Hello " + name;
-}
-var mesaj = greeter("burak");
-console.log(mesaj);
