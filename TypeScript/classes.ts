c08ace4e019957767385d5ccf1e305f876e13147
BurakYavas <burakyavas26@gmail.com>
2023-01-05 05:12:56 +0300

    Inheritance

--- TypeScript/classes.ts
+++ TypeScript/classes.ts
@@ -11,3 +11,30 @@ class Home {
 let home = new Home(3,4,5);
 
 console.log(home._floor)
+
+class Kisi{
+    kaydet(){
+        console.log("Kisi kaydedildi")
+    }
+}
+
+class Musteri extends Kisi{
+    satisYap(){
+        console.log("Satis yapildi")
+    }
+}
+
+class Personel extends Kisi{
+    maasOde(){
+        console.log("Maas odendi")
+    }
+}
+
+let personel = new Personel();
+let musteri = new Musteri();
+
+personel.maasOde();
+personel.kaydet();
+
+musteri.satisYap();
+musteri.kaydet();
