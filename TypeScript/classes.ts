3ae22e48b57ba92bb3a16cb2a71ef06d6cdb2be8
BurakYavas <burakyavas26@gmail.com>
2023-01-06 15:42:03 +0300

    Getter and Setter

--- TypeScript/classes.ts
+++ TypeScript/classes.ts
@@ -13,36 +13,56 @@ let home = new Home(3,4,5);
 console.log(home._floor)
 
 
+
+
 class Kisi{
+
     //Tanimlandigi class ve Inherit eden classda 
     protected nameProtected:string
     //private tanimlandigi classdan
     private _namePrivate:string
     //Public heryerden
     public namePublic:string
+
+    //Getter ve Setter
+
+    get name():string{
+        return "Sayin :"+this._namePrivate
+    }
+
+    set name(newName:string){
+        this._namePrivate = newName
+    }
+
+
     kaydet(){
-        console.log("Kisi kaydedildi")
-        console.log(this._namePrivate)
+        
+        console.log(`${this.name} kaydedildi`)
+        //console.log(this._namePrivate)
     }
 }
 
 class Musteri extends Kisi{
     satisYap(){
         
-        console.log(this.nameProtected)
-        console.log(this.namePublic)
-        console.log("Satis yapildi")
+        //console.log(this.nameProtected)
+        //console.log(this.namePublic)
+        console.log(`${this.name} Satis yapildi`)
+        //alert(`${this.name} Satis yapildi`)
     }
 }
 
 class Personel extends Kisi{
     maasOde(){
-        console.log("Maas odendi")
+        console.log(`${this.name} Maas odendi`)
     }
 }
 
 let personel = new Personel();
 let musteri = new Musteri();
+
+musteri.name = "Burak"
+personel.name = "Meltem"
 
 personel.maasOde();
 personel.kaydet();
